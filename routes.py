a9b267f029b8789f1014ce1e6961c305b7b3265a
george udosen <datameshprojects@gmail.com>
2024-08-07 14:26:15 +0100

    import body module

--- routes.py
+++ routes.py
@@ -1,10 +1,37 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Body
+from models.playersModels import Player
+from bson import ObjectId
 
 
 router = APIRouter(prefix="", tags=['players'])
 
 
 @router.get("/")
-async def getPlayers(request: Request):
-    print(request.app.players)
-    return {"message": "Hello World!!"}
+async def getPlayers(request: Request) -> list[Player]:
+    db = request.app.players
+    response = list(db.find({}))
+    for item in response: 
+        item["_id"] = str(item["_id"])
+    return response
+
+
+
+@router.post("/")
+async def addPlayer(request: Request, player: Player = Body(...)):
+    db = request.app.players
+    response = db.insert_one(player.model_dump())
+    return {"id": str(response.inserted_id)}
+
+@router.delete("/{id}")
+async def deletePlayer(request: Request, id):
+    _id = ObjectId(id)
+    db = request.app.players
+    response = db.delete_one({"_id": _id})
+    return {"deleted_count": response.deleted_count}
+
+@router.put("/{id}")
+async def updatePlayer(request: Request, id, player: Player = Body(...)):
+    _id = ObjectId(id)
+    db = request.app.players
+    response = db.update_one({"_id": _id}, {"$set": player.model_dump()})
+    return {"updated_count": response.modified_count}
